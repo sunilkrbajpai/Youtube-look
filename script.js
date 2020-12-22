5dc6a84ca3eb311fc49d5f560a715ef25337a1a8
Sunil kumar bajpai <sunilkumarbajpai.skb@gmail.com>
2020-12-22 15:42:14 +0530

    completed

--- script.js
+++ script.js
@@ -1,197 +1,196 @@
-// Dummy data for videos and comments
+// Dummy data for recommended videos and comments
 
-var video = [
+var videos = [
         {
             "title": "Cats and Invisible Wall Compilation",
-            "uploader": "Munchkin",
-            "views": "17M views • 8 months ago",
+            "channel": "Munchkin",
+            "info": "17M views • 8 months ago",
             "thumbnail": "https://img.youtube.com/vi/EJS4hYNngto/maxresdefault.jpg"
         },
         {
             "title": "Cute and Funny Cat Videos to Keep You Smiling! 🐱",
-            "uploader": "Rufus",
-            "views": "16.8M views • 8 months ago",
+            "channel": "Rufus",
+            "info": "16.8M views • 8 months ago",
             "thumbnail": "https://img.youtube.com/vi/tpiyEe_CqB4/maxresdefault.jpg"
         },
         {
             "title": "TypeScript Course for Beginners 2020 - Learn TypeScript from Scratch!",
-            "uploader": "Academind",
-            "views": "462,610 views • Jan 21, 2020",
+            "channel": "Academind",
+            "info": "462,610 views • Jan 21, 2020",
             "thumbnail": "https://img.youtube.com/vi/BwuLxPH8IDs/maxresdefault.jpg"
         },
         {
             "title": "Introduction to React",
-            "uploader": "Le Wagon",
-            "views": "51,738 views • Oct 26, 2017",
+            "channel": "Le Wagon",
+            "info": "51,738 views • Oct 26, 2017",
             "thumbnail": "https://img.youtube.com/vi/_ZTT9kw3PIE/0.jpg"
         },
         {
             "title": "Cats and Domino",
-            "uploader": "Cat Navi Desk",
-            "views": "35,172,414 views • Mar 14, 2020",
+            "channel": "Cat Navi Desk",
+            "info": "35,172,414 views • Mar 14, 2020",
             "thumbnail": "https://img.youtube.com/vi/7Nn7NZI_LN4/maxresdefault.jpg"
         },
         {
             "title": "Why am I not at the Olympics?",
-            "uploader": "CatPussic",
-            "views": "25,068,492 views • May 18, 2018",
+            "channel": "CatPussic",
+            "info": "25,068,492 views • May 18, 2018",
             "thumbnail": "https://img.youtube.com/vi/Mfi0NMtfUzI/maxresdefault.jpg"
         },
         {
             "title": "Funny Cats Compilation (Most popular) Part 2",
-            "uploader": "NoCat NoLife",
-            "views": "4.6M . 4 years ago",
+            "channel": "NoCat NoLife",
+            "info": "4.6M . 4 years ago",
             "thumbnail": "https://img.youtube.com/vi/NsUWXo8M7UA/maxresdefault.jpg"
         },
         {
             "title": "Waiter! Bring me food!",
-            "uploader": "CatPussic",
-            "views": "33,443,085 views • Jun 30, 2017",
+            "channel": "CatPussic",
+            "info": "33,443,085 views • Jun 30, 2017",
             "thumbnail": "https://img.youtube.com/vi/k0QhnMJnJE0/maxresdefault.jpg"
         }
     ];
 
 var comments = [
     {
-        "image":"https://yt3.ggpht.com/ytc/AAUvwnhoNAFrF8hYj0zCYCfCXsarW81jgX-c0rCH_vyymQ=s48-c-k-c0xffffffff-no-rj-mo",
+        "thumbnail":"https://yt3.ggpht.com/ytc/AAUvwnhoNAFrF8hYj0zCYCfCXsarW81jgX-c0rCH_vyymQ=s48-c-k-c0xffffffff-no-rj-mo",
         "name":"Ray Mak",
         "comment": "I miss this meme a lot. It's back in my recommendation.",
     },
     {
-        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSaaU1bFocDOx3Q8zh35970WbOSDN-n-x_Whg&usqp=CAU",
+        "thumbnail":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSaaU1bFocDOx3Q8zh35970WbOSDN-n-x_Whg&usqp=CAU",
         "name":"Steam Pw",
         "comment": "When the world needed him the most, he returned.",
     },
     {
-        "image":"https://yt3.ggpht.com/ytc/AAUvwni3woc15AzPA4Ti4LqTMQV7XnfcMOl0JT-oGCbK=s48-c-k-c0xffffffff-no-rj-mo",
+        "thumbnail":"https://yt3.ggpht.com/ytc/AAUvwni3woc15AzPA4Ti4LqTMQV7XnfcMOl0JT-oGCbK=s48-c-k-c0xffffffff-no-rj-mo",
         "name":"Triplet",
         "comment": "This is literally one of the ancient fragments of the internet.",
     },
 ]
 
 
-// fetching required elements form HTML doc
+// gettind elements from DOM
 
 const content = document.getElementById("content");
 const showMore = document.getElementById("show-more");
-const subs = document.getElementById("subscribe-btn");
-const aside = document.getElementById("videolist");
-const comSec = document.getElementById("comment-section");
+const subscribe = document.getElementById("subscribe-btn");
+const recommendation = document.getElementById("recommendations");
+const commentSection = document.getElementById("comment-section");
 
-showMore.state = "show";
+// show more text function
+
+showMore.state = "false";
 showMore.onclick = function(){
 
-    if(showMore.state=="show"){
+    if(showMore.state=="false"){
         content.style.height = "auto";
-        showMore.state = "hide";
+        showMore.state = "true";
         showMore.innerHTML = "SHOW LESS";
     }
     else{
         content.style.height = "120px";
-        showMore.state = "show";
+        showMore.state = "false";
         showMore.innerHTML = "SHOW MORE";
     }
 }
 
-subs.state = "subbed";
-subs.onclick = function(){
+// subscribe button function
 
-    if(subs.state=="subbed"){
-        subs.style.backgroundColor="grey";
-        subs.innerHTML = "SUBSCRIBED";
-        subs.state="notSubbed"
+subscribe.state = "false";
+subscribe.onclick = function()
+{
+    if(subscribe.state=="false"){
+        subscribe.style.backgroundColor="grey";
+        subscribe.innerHTML = "SUBSCRIBED";
+        subscribe.state="true"
     }
     else{
-        subs.style.backgroundColor="#cc0000";
-        subs.innerHTML = "SUBSCRIBE";
-        subs.state = "subbed";
+        subscribe.style.backgroundColor="#cc0000";
+        subscribe.innerHTML = "SUBSCRIBE";
+        subscribe.state = "false";
     }
 }
 
-//render list of video cards in the aside section 
+//Render recommended videos
 
-for(var i=0; i<video.length; i++){
+for(var i=0; i<videos.length; i++)
+{
     var card = document.createElement('div');
     card.classList.add('card');
 
     var cardImage = document.createElement('img');
-    cardImage.classList.add('cardImg');
-    cardImage.src = video[i].thumbnail;
+    cardImage.classList.add('cardImage');
+    cardImage.src = videos[i].thumbnail;
     card.appendChild(cardImage);
 
     var cardInfo = document.createElement('div');
     cardInfo.classList.add('cardInfo');
     card.appendChild(cardInfo);
 
-    var cardMenu = document.createElement('div');
-    cardMenu.classList.add('cardMenu');
-    card.appendChild(cardMenu);
-    
     var cardTitle = document.createElement('div');
-    cardTitle.innerHTML = video[i].title;
+    cardTitle.innerHTML = videos[i].title;
     cardTitle.classList.add('cardTitle');
     cardInfo.appendChild(cardTitle);
 
-    var uploader = document.createElement('div');
-    uploader.innerHTML = video[i].uploader;
-    uploader.classList.add('uploader');
-    cardInfo.appendChild(uploader);
+    var channel = document.createElement('div');
+    channel.innerHTML = videos[i].channel;
+    channel.classList.add('channel-name');
+    cardInfo.appendChild(channel);
 
-    var views = document.createElement('div');
-    views.innerHTML = video[i].views;
-    views.classList.add('views');
-    cardInfo.appendChild(views);
+    var info = document.createElement('div');
+    info.innerHTML = videos[i].info;
+    info.classList.add('moreInfo');
+    cardInfo.appendChild(info);
 
-    aside.appendChild(card);
+    recommendation.appendChild(card);
 
     if(i==0){
         var hr = document.createElement('div');
         hr.classList.add('hr-line');
-        aside.appendChild(hr);
+        hr.classList.add('right-hr');
+        recommendation.appendChild(hr);
     }
-
 }
 
-//render comments
+//render comments section
 
-for(var i=0; i<comments.length; i++){
-    console.log(comments[i]);
-    
+for(var i=0; i<comments.length; i++)
+{  
     var commentCard = document.createElement('div');
     commentCard.classList.add('commentCard');
 
     var img = document.createElement('img');
-    img.classList.add('commentImg');
-    img.src = comments[i].image;
+    img.classList.add('commentImage');
+    img.src = comments[i].thumbnail;
 
-    var commentInfo = document.createElement('div');
-    commentInfo.classList.add('commentInfo');
+    var commentText = document.createElement('div');
+    commentText.classList.add('commentText');
 
-    var commenterName = document.createElement('div');
-    commenterName.innerHTML = comments[i].name;
-    commentInfo.appendChild(commenterName);
-    commenterName.style.fontSize = "1rem";
-    commenterName.style.fontWeight = "900";
-    commenterName.style.paddingBottom = "5px";
+    var Name = document.createElement('div');
+    Name.innerHTML = comments[i].name;
+    commentText.appendChild(Name);
+    Name.style.fontSize = "0.8rem";
+    Name.style.fontWeight = "900";
+    Name.style.paddingBottom = "5px";
 
     var comment = document.createElement('div');
     comment.innerHTML = comments[i].comment;
-    commentInfo.appendChild(comment);
+    commentText.appendChild(comment);
 
     var actions = document.createElement('div');
     actions.classList.add('actions');
 
-    var like = document.createElement('div');
-    like.innerHTML = '<i class="fas fa-thumbs-up"></i> 96K';
-    actions.appendChild(like);
+    var likes = document.createElement('div');
+    likes.innerHTML = '<i class="fas fa-thumbs-up"></i> 96K';
+    actions.appendChild(likes);
 
-    var dislike = document.createElement('div');
-    dislike.innerHTML = '<i class="fas fa-thumbs-down"></i>';
-    actions.appendChild(dislike);
-
-    commentInfo.appendChild(actions);
+    var dislikes = document.createElement('div');
+    dislikes.innerHTML = '<i class="fas fa-thumbs-down"></i>';
+    actions.appendChild(dislikes);
     commentCard.appendChild(img);
-    commentCard.appendChild(commentInfo);
+    commentText.appendChild(actions);
+    commentCard.appendChild(commentText);
     
-    comSec.appendChild(commentCard);
+    commentSection.appendChild(commentCard);
 }
